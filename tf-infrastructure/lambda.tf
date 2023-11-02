79cb74f1ddcd8f1615a887e9d8977401bf80a9e0
Paul Moros Anacona <paul.moros@globant.com>
2023-11-02 09:37:39 -0500

    fix: image_uri in lambda

--- tf-infrastructure/lambda.tf
+++ tf-infrastructure/lambda.tf
@@ -2,7 +2,7 @@ resource "aws_lambda_function" "container_lambda" {
   function_name = "${var.app_name}_${var.environment}_container_lambda"
   role          = aws_iam_role.lambda_exec_role.arn
   handler       = "app.handler" # Adjust the handler based on your app
-  image_uri     = aws_ecr_repository.ecr_repository.repository_url
+  image_uri     = "${aws_ecr_repository.ecr_repository.repository_url}:latest"
 
   package_type = "Image"
   timeout      = 10
