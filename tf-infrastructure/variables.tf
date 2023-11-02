35534c6778bc8784ae7b86e04f2c23bed8987d82
Paul Moros Anacona <paul.moros@globant.com>
2023-11-02 09:06:55 -0500

    fix: add destroy in iac branch

--- tf-infrastructure/variables.tf
+++ tf-infrastructure/variables.tf
@@ -10,5 +10,6 @@ variable "environment" {
 
 variable "app_name" {
   type    = string
-  default = "myapp"
+  # prefixes with a random string to avoid collisions
+  default = "my_app_${random_string.random_suffix.result}"
 }
