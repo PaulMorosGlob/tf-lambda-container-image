d395f936cfbf9fca2465e72f534b3b72b017935b
Paul Moros Anacona <paul.moros@globant.com>
2023-11-02 09:08:32 -0500

    fix: change default app name

--- tf-infrastructure/variables.tf
+++ tf-infrastructure/variables.tf
@@ -11,5 +11,5 @@ variable "environment" {
 variable "app_name" {
   type    = string
   # prefixes with a random string to avoid collisions
-  default = "my_app_${random_string.random_suffix.result}"
+  default = "myapp"
 }
